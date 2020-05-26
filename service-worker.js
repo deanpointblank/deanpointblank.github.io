5ae687e8cee0972089b9e38c727f4e9a826154c7
Dean <deanlloyd64@gmail.com>
2020-05-25 22:17:38 -0400

    minor fixes, grammatical errors fixed, video scaling somewhat addressed.

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.87053ee17dead53c7ae2cc16f5f2763b.js"
+  "/precache-manifest.28694a68cd9ecaaee0a11a1dd77c7d51.js"
 );
 
 self.addEventListener('message', (event) => {
